ab16f29c3be042870ab5067ca181fa96d7e46c26
Muhammet-Tarik-Dumanli <mtdumanli@gmail.com>
2024-01-10 22:17:37 +0300

    fixed

--- WebApi/Controllers/CurrenciesController.cs
+++ WebApi/Controllers/CurrenciesController.cs
@@ -36,12 +36,11 @@ public class CurrenciesController : ControllerBase
     [HttpGet]
     public IActionResult GetLatestExchangeRates(string baseCurrency = "USD")
     {
-        var client = new RestClient($"{Config.BaseUrl}/economy/currencyToAll");
+        var client = new RestClient($"{Config.BaseUrl}/currencyToAll?int=10&base=USD");
         var request = new RestRequest();
         request.AddHeader("authorization", $"apikey {Config.APIKEY}");
         request.AddHeader("content-type", "application/json");
         request.AddParameter("int", "10");
-        request.AddParameter("base", baseCurrency);
 
         RestResponse response = client.Execute(request);
 
@@ -58,7 +57,7 @@ public class CurrenciesController : ControllerBase
     [HttpGet("allCurrency")]
     public IActionResult GetAllCurrencies()
     {
-        var client = new RestClient($"{Config.BaseUrl}/economy/allCurrency");
+        var client = new RestClient($"{Config.BaseUrl}/allCurrency");
         var request = new RestRequest();
         request.AddHeader("authorization", $"apikey {Config.APIKEY}");
         request.AddHeader("content-type", "application/json");
